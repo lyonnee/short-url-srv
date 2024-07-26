6352c9dfeab7c498c31d3436b75ca000f1a845ef
lyonnee <lyon.nee@outlook.com>
2024-07-26 17:46:37 +0800

    feat: jwt
    feat: jwt
    
    feat: jwt

---
+++ src/infra/auth/jwt.rs
@@ -0,0 +1,62 @@
+use axum::{async_trait, extract::FromRequestParts, http::StatusCode};
+use jsonwebtoken::{errors::Error, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+
+use crate::infra::utils::time;
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub exp: usize, // Required (validate_exp defaults to true in validation). Expiration time (as UTC timestamp)
+    pub iat: usize, // Issued at (as UTC timestamp)
+    pub iss: String, // Optional. Issuer
+    pub uid: usize, // user id
+    pub jti: usize, // Optional. jwt id
+}
+
+impl Claims {
+    pub fn new(uid: usize) -> Self {
+        let iat = (time::timestamp_millis()) as usize;
+        let exp = iat + 10;
+
+        Self {
+            iat: iat,
+            exp: exp,
+            iss: String::from(""),
+            uid: uid,
+            jti: 0,
+        }
+    }
+}
+
+pub struct TokenPayload{
+    pub access_token: String,
+    pub token_type: String,
+}
+
+pub fn create_token(uid: usize) -> Result<TokenPayload, Error> {
+    let encoding_key: EncodingKey = EncodingKey::from_secret("".as_bytes());
+    let token = sign(uid, &encoding_key)?;
+
+    Ok(TokenPayload{
+        access_token: token,
+        token_type: "Bearer".to_string(),
+    })
+}
+
+pub fn verify(token: &str) -> Result<Claims, Error> {
+    let decode_key = &DecodingKey::from_secret("".as_bytes());
+
+    Ok(
+        jsonwebtoken::decode(token, decode_key, &Validation::default())
+            .map(|data: jsonwebtoken::TokenData<Claims>| data.claims)?,
+    )
+}
+
+fn sign(uid: usize, encoding_key: &EncodingKey) -> Result<String, Error> {
+    Ok(jsonwebtoken::encode(
+        &Header::default(),
+        &Claims::new(uid),
+        encoding_key,
+    )?)
+}
--- src/logic/user_logic.rs
+++ src/logic/user_logic.rs
@@ -1,35 +1,45 @@
-use bcrypt::{DEFAULT_COST,hash_with_salt,verify};
-use rand::{RngCore,rngs::OsRng};
+use std::fmt::format;
 
-use crate::{infra::db, repository::user_repo};
+use bcrypt::{hash_with_salt, verify, DEFAULT_COST};
+use rand::{rngs::OsRng, RngCore};
+
+use crate::{
+    infra::{
+        auth::jwt::{self, TokenPayload},
+        db,
+    },
+    repository::user_repo,
+};
 
 pub async fn register_new(
     email: Option<String>,
     phone: Option<String>,
     password: String,
-) -> Result<u64,String>{
+) -> Result<u64, String> {
     let mut tx = db::begin_db_transaction().await;
-    let user = user_repo::find_user_by_phone_or_email(&mut *tx,email.clone(),phone.clone()).await;
-    if let Some(_) = user{
+    let user = user_repo::find_user_by_phone_or_email(&mut *tx, email.clone(), phone.clone()).await;
+    if let Some(_) = user {
         let _ = tx.rollback().await;
-        return  Err(String::from("email or phone has used"));
+        return Err(String::from("email or phone has used"));
     }
 
-    let salt =  generate_random_bytes();
-    let hashed_pwd = hash_with_salt(password, DEFAULT_COST, salt).unwrap().to_string();
-
+    let salt = generate_random_bytes();
+    let hashed_pwd = hash_with_salt(password, DEFAULT_COST, salt)
+        .unwrap()
+        .to_string();
 
     let salt_str = String::from_utf8_lossy(&salt);
-    let res = user_repo::create_user( &mut *tx,email,phone, salt_str.to_string(),hashed_pwd).await;
+    let res =
+        user_repo::create_user(&mut *tx, email, phone, salt_str.to_string(), hashed_pwd).await;
 
     let commit_res = tx.commit().await;
 
-    match commit_res{
-        Ok(_)=> Ok(res.unwrap()),
+    match commit_res {
+        Ok(_) => Ok(res.unwrap()),
         Err(err) => {
             // todo: 记录失败原因日志
             Err(err.to_string())
-        },
+        }
     }
 }
 
@@ -37,23 +47,37 @@ pub async fn login(
     email: Option<String>,
     phone: Option<String>,
     password: String,
-) -> bool{
+) -> Result<TokenPayload, String> {
     let mut conn = db::get_db_conn().await;
-    let user = user_repo::find_user_by_phone_or_email(&mut *conn,email,phone).await;
+    let res = user_repo::find_user_by_phone_or_email(&mut *conn, email, phone).await;
 
-    if let Some(user) = user{
-        let hash = user.ciphertext.unwrap();
-        let res = verify(password,&hash);
+    match res {
+        Some(user) => {
+            let hash = user.ciphertext.unwrap();
 
-        if let Ok(pass) = res {
-            return pass;
+            let verify_res = verify(password, &hash);
+            match verify_res {
+                Ok(pass) => match pass {
+                    true => {
+                        let create_jwt_res = jwt::create_token(user.id.unwrap() as usize);
+                        match create_jwt_res {
+                            Ok(token) => Ok(token),
+                            Err(e) => {
+                                tracing::error!("{}", e);
+                                Err(format!("{}", e))
+                            }
+                        }
+                    }
+                    false => Err(String::from("the password is incorrect")),
+                },
+                Err(e) => {
+                    tracing::error!("{}", e);
+                    Err(format!("{}", e))
+                }
+            }
         }
-
-        // TODO: 记录错误res.error
-        return false;
+        None => Err(String::from("user not found")),
     }
-
-    return  false;
 }
 
 fn generate_random_bytes() -> [u8; 16] {
